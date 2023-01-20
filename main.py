e3b6dcfe457a466c95066d374bbb4abc021f790b
LazyCoder-1506 <pranayan1506@gmail.com>
2023-01-21 04:04:14 +0530

    added check for input file

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 import json
-import datetime
-import base64
+from datetime import datetime
+from base64 import b64decode
+import os
+from sys import exit
 
 def getUUID(s):
   return s.split('uuid:', 1)[1].split(',', 1)[0]
   
-
-with open('./data/data.json', 'r') as inputfile:
+input_path = './data/data.json'
+if os.path.isfile(input_path) == False:
+  exit('No input file found')
+with open(input_path, 'r') as inputfile:
   input_object = json.load(inputfile)
 
-current_time = datetime.datetime.now()
-current_unix = int(datetime.datetime.timestamp(current_time))
+current_time = datetime.now()
+current_unix = int(datetime.timestamp(current_time))
 
 value_sum = 0
 uuid_array = []
 
 for device in input_object['Devices']:
   if current_unix <= int(device['timestamp']):
-    value_sum += int.from_bytes(base64.b64decode(device['value']), 'big')
+    value_sum += int.from_bytes(b64decode(device['value']), 'big')
     uuid = getUUID(device['Info'])
     uuid_array.append(uuid)
 
